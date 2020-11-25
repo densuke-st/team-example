1e468d9772be9d38d5aae1faee5f94a1afd12bef
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2020-11-25 13:20:21 +0900

    メソッドから出力系を取得

--- team-example/src/HelloWorld.java
+++ team-example/src/HelloWorld.java
@@ -13,7 +13,11 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println(hw());
+	}
+	
+	public static String hw() {
+		return "Hello, World from method.";
 	}
 
 }
