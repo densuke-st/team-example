0cf1f6e8580cdf4ba37342591c67550f98b59228
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2020-11-26 10:14:56 +0900

    mainのTODO完了

--- team-example/src/HelloWorld.java
+++ team-example/src/HelloWorld.java
@@ -12,10 +12,9 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		System.out.println(hw());
 	}
-	
+
 	public static String hw() {
 		return "Hello, World! from another user.";
 	}
